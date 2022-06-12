763af1b4a8cd657b79822f797ed43d815ad92635
youngtemmy <alukotemitope49@gmail.com>
2022-06-12 14:42:57 +0100

    first commit

---
+++ routes/products.js
@@ -0,0 +1,10 @@
+
+    const express = require('express');
+    const router = express.Router();
+    // import controllers
+    const { getAllProductsStatic, getAllProducts, createProduct } = require('../controllers/products');
+
+    router.route('/').get(getAllProducts).post(createProduct);
+    router.route('/static').get(getAllProductsStatic);
+
+    module.exports = router
