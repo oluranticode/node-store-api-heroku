b8dfbf3d9cad19e308f0baf50a059cac2abf6c43
youngtemmy <alukotemitope49@gmail.com>
2022-06-13 04:41:23 +0100

    update

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+    // Swagger
+const SwaggerUI = require('swagger-ui-express');
+const YAML = require('yaml-js');
+const swaggerDocs = YAML.load('./swagger.yaml');
+
 const app = express();
 // import middlewares 
 const notfoundMiddleware = require('./middleware/not-found');
@@ -20,12 +25,6 @@ const router = require('./routes/products');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
-// route
-app.get('/', (req, res) => {
-    res.send('<h1> API STORE </h1><a href="/api/v1/products"> products API </a>')
-})
-
 app.set('trust proxy', 1);
   app.use(
     rateLimiter({
@@ -37,6 +36,13 @@ app.set('trust proxy', 1);
   app.use(helmet());
   app.use(cors());
   app.use(xss()); 
+
+// route
+app.get('/', (req, res) => {
+    res.send('<h1> API STORE </h1><a href="/api/v1/products"> products API </a><br> <a href="/api-docs"> SwaggerUI </a>')
+})
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 // products route
 app.use('/api/v1/products', router)
