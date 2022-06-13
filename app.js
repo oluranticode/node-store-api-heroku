713faa7b0a336ab35a332030a942af80d27928ed
youngtemmy <alukotemitope49@gmail.com>
2022-06-13 03:04:16 +0100

    third commit

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('dotenv').config()
 require('express-async-errors');
 // import express
 const express = require('express');
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const app = express();
 // import middlewares 
 const notfoundMiddleware = require('./middleware/not-found');
@@ -19,6 +25,18 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<h1> API STORE </h1><a href="/api/v1/products"> products API </a>')
 })
+
+app.set('trust proxy', 1);
+  app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    })
+  );
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors());
+  app.use(xss()); 
 
 // products route
 app.use('/api/v1/products', router)
